580e6d17c1a51e8641cfc247b625d259cbd3d23d
Yusu <pwc1317@gmail.com>
2024-07-12 13:52:00 +0800

    gitee test commit

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello, world!");
         System.out.println("Hello, yusu!");
         System.out.println("push test");
+        System.out.println("gitee test");
     }
 }
